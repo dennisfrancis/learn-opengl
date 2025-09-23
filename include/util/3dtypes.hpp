d3430180199c6e7bdd6a596df2e8e023afea1bfb
Dennis Francis <dennisfrancis.in@gmail.com>
2025-09-23 22:53:09 +0530

    ogldev: add deg <=> radian conversion functions
    Signed-off-by: Dennis Francis <dennisfrancis.in@gmail.com>

--- include/util/3dtypes.hpp
+++ include/util/3dtypes.hpp
@@ -1,6 +1,20 @@
+#pragma once
+
+#include <numbers>
 
 namespace util
 {
+
+constexpr inline float to_radian(float x)
+{
+    return x * std::numbers::pi_v<float> / 180.0f;
+}
+
+constexpr inline float to_degree(float x)
+{
+    return x * 180.0f / std::numbers::pi_v<float>;
+}
+
 using mat4x4f_t = float[4][4];
 
 struct Mat4x4f
